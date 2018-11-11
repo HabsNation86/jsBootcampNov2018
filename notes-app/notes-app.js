43f0f4dff93869b035b8d56d738d0df67117537b
Jamie <jamiemckeown469@gmail.com>
2018-11-11 09:55:59 -0500

    eventListener

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,19 +1,38 @@
+const notes = [{
+  title: 'book1',
+  body: 'random info'
+}, {
+  title: 'book2',
+  body: 'generic information'
+}, {
+  title: 'book3',
+  body: ' Cant get a job'
+}]
+
+
+
+
 // how to remove element
 
 // const h1 = document.querySelector('h1')
 // h1.remove()
 
 // query all and remove
-const p = document.querySelectorAll('p')
-  p.forEach(function(p){
-  console.log(p.textContent)
-  p.textContent = 'Rambo'
+// const p = document.querySelectorAll('p')
+//   p.forEach(function(p){
+//   console.log(p.textContent)
+//   p.textContent = 'Rambo'
 
-})
+// })
 
 
 //  Add a new element
 
-const newParagraph = document.createElement('p')
-newParagraph.textContent = 'This is a new element from javascript'
-document.querySelector('body').appendChild(newParagraph)
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'This is a new element from javascript'
+// document.querySelector('body').appendChild(newParagraph)
+
+document.querySelector('button').addEventListener('click', function (e) {
+  e.target.textContent = 'The button was clicked'
+
+})
--- todo-app/todos.js
+++ todo-app/todos.js
@@ -25,16 +25,36 @@ const todos = [{
 // You have 2 todo's left (p element)
 //  add a p for each todo above ( use text value)
 
+
+
+// getting incomplete todos
 const incompleteTodos =  todos.filter(function(todo){
+// since we want incomplete we ask for the OPPOSITE (!) of complete
   return !todo.completed
 })
 
+
+// create an h2 and name it summary
 const summary = document.createElement('h2')
+// add content to the h2 we created (now known as summary)
 summary.textContent = `You have ${incompleteTodos.length} todos left`
+// add the content (summary) into child element of "body"
 document.querySelector('body').appendChild(summary)
 
+
+
+// for each todo item...
 todos.forEach(function(todo){
-  const p =document.createElement('p')
+// create a paragraph "p"
+  const p = document.createElement('p')
+// inject the text from each todo into the "p" created previously
   p.textContent = todo.text
+// add the content "p" to the child element of body
   document.querySelector('body').appendChild(p)
+})
+
+// add a button, make click listener and send message to console
+
+document.querySelector('button').addEventListener("click", function(){
+  console.log('aint nobody messing with my click')
 })
